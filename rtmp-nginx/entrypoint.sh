1a15cb2c08a76dd792079ff58ee779fae1a755b4
Kelton Person <kelton.person@gmail.com>
2016-11-05 13:40:26 -0500

    first nginx rtmp

---
+++ rtmp-nginx/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+h=${HLS_ON:-true}
+d=${DASH_ON:-true}
+
+if [ $h == "true" ]; then
+    export HLS=true
+fi
+
+if [ $d == "true" ]; then
+    export DASH=true
+fi
+
+export STREAM=${STREAM_APP:-stream}
+confd -onetime -backend env
+exec $@
