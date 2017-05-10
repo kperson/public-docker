fe0bc6e8783e56a840337494b028970c6f6153a0
Kelton Person <kelton.person@gmail.com>
2017-05-10 06:43:46 -0500

    added reverse sshd server

---
+++ sshd-reverse/entrypoint.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+
+set -e
+
+[ "$DEBUG" == 'true' ] && set -x
+
+DAEMON=sshd
+
+# Copy default config from cache
+if [ ! "$(ls -A /etc/ssh)" ]; then
+   cp -a /etc/ssh.cache/* /etc/ssh/
+fi
+
+# Generate Host keys, if required
+if ! ls /etc/ssh/ssh_host_* 1> /dev/null 2>&1; then
+    ssh-keygen -A
+fi
+
+# Fix permissions, if writable
+if [ -w ~/.ssh ]; then
+    chown root:root ~/.ssh && chmod 700 ~/.ssh/
+fi
+if [ -w ~/.ssh/authorized_keys ]; then
+    chown root:root ~/.ssh/authorized_keys
+    chmod 600 ~/.ssh/authorized_keys
+fi
+if [ -w /etc/authorized_keys ]; then
+    chown root:root /etc/authorized_keys
+    chmod 755 /etc/authorized_keys
+    find /etc/authorized_keys/ -type f -exec chmod 644 {} \;
+fi
+
+# Add users if SSH_USERS=user:uid:gid set
+if [ -n "${SSH_USERS}" ]; then
+    USERS=$(echo $SSH_USERS | tr "," "\n")
+    for U in $USERS; do
+        IFS=':' read -ra UA <<< "$U"
+        _NAME=${UA[0]}
+        _UID=${UA[1]}
+        _GID=${UA[2]}
+
+        echo ">> Adding user ${_NAME} with uid: ${_UID}, gid: ${_GID}."
+        if [ ! -e "/etc/authorized_keys/${_NAME}" ]; then
+            echo "WARNING: No SSH authorized_keys found for ${_NAME}!"
+        fi
+        addgroup -g ${_GID} ${_NAME}
+        adduser -D -u ${_UID} -G ${_NAME} -s '' ${_NAME}
+    done
+else
+    # Warn if no authorized_keys
+    if [ ! -e ~/.ssh/authorized_keys ] && [ ! $(ls -A /etc/authorized_keys) ]; then
+      echo "WARNING: No SSH authorized_keys found!"
+    fi
+fi
+
+# Update MOTD
+if [ -v MOTD ]; then
+    echo -e "$MOTD" > /etc/motd
+fi
+
+stop() {
+    echo "Received SIGINT or SIGTERM. Shutting down $DAEMON"
+    # Get PID
+    pid=$(cat /var/run/$DAEMON/$DAEMON.pid)
+    # Set TERM
+    kill -SIGTERM "${pid}"
+    # Wait for exit
+    wait "${pid}"
+    # All done.
+    echo "Done."
+}
+
+echo "Running $@"
+if [ "$(basename $1)" == "$DAEMON" ]; then
+    trap stop SIGINT SIGTERM
+    $@ &
+    pid="$!"
+    mkdir -p /var/run/$DAEMON && echo "${pid}" > /var/run/$DAEMON/$DAEMON.pid
+    wait "${pid}" && exit $?
+else
+    exec "$@"
+fi
+
+exec "$@"
